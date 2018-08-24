becbb3ca1ddad25f78e8b15ea875e2fcf157c768
Broderick Hyman <broderickhyman@gmail.com>
2018-08-24 18:49:02 -0400

    Adding nuget packages and command line parsing

--- albiondata-deduper-dotNet/Program.cs
+++ albiondata-deduper-dotNet/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
 
 namespace albiondata_deduper_dotNet
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [Option(Description = "Redis URL", ShortName = "r", ShowInHelpText = true)]
+        public string RedisAddress { get; } = "localhost:6379";
+
+        [Option(Description = "Redis Password", ShortName = "p", ShowInHelpText = true)]
+        public string RedisPassword { get; } = "";
+
+        [Option(Description = "NATS Url", ShortName = "n", ShowInHelpText = true)]
+        public string NatsUrl { get; } = "nats://localhost:4222";
+
+        private void OnExecute()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(RedisAddress);
+            Console.WriteLine(RedisPassword);
+            Console.WriteLine(NatsUrl);
+
+            Console.ReadKey();
         }
     }
 }
